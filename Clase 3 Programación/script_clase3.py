754612e929ba28c5606aeb326e0b694fad9e4014
lujangarrigo <garrigo.lujan@gmail.com>
2023-03-31 04:40:28 -0300

    elimino archivos innecesarios

--- Clase 3 Programación/script_clase3.py
+++
@@ -1,8 +0,0 @@
-arch = open("hola.txt","r")
-print(arch) #Si no pongo read, no me va a imprimir el contenido
-"""print(arch.read())""" #Solo si pongo read me va a imprimir el contenido del archivo
-#Para ejecutar tengo q poner en la terminal "Python scr + tab"
-#Read lee todo el archivo como si fuese un unico string
-print(arch.readlines())#Con readlines() lee linea a linea el archivo. Te lo devuelve como lista
-#El \n significa salto de linea
-#Puedo iterar linea a linea el texto
