5145bd694be403661c36f269b551a572b7fbba88
lujangarrigo <garrigo.lujan@gmail.com>
2023-04-14 16:46:43 -0300

    Práctica expresiones

--- este no_Clases_Informática/3_Clase_Rutas_os_globo/Práctica/ej2_clase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#Escribí un programa que lea un archivo e imprima las primeras n líneas.
-
-def read_n_lines(n,archivo):
-    with open(archivo,"r") as file:
-        for i in range(n): #range(a,b,c) a =inicio , b=fin, c=paso (de a cuantas lineas)
-            print(file.readline())
-read_n_lines(2,"archivo1.txt")
