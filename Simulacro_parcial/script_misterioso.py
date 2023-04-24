e4b63b66748524ac51c18381e2841b52c6852df4
lujangarrigo <garrigo.lujan@gmail.com>
2023-04-24 11:02:21 -0300

    intento ej4

---
+++ Simulacro_parcial/script_misterioso.py
@@ -0,0 +1,20 @@
+def trasladar(una_lista, otra_lista, elemento):
+  otra_lista.append(elemento)
+  una_lista.remove(elemento)
+
+lista = [2,5,8]
+listita = []
+trasladar(listita, lista, 2)
+
+# B) -Error: ValueError
+
+def trasladar(una_lista, otra_lista, elemento):
+    try:
+      una_lista.remove(elemento)
+      otra_lista.append(elemento)
+    except ValueError:
+        print ("El elemento que se quiere eliminar no está en la lista")
+
+lista = [2,5,8]
+listita = []
+trasladar(listita, lista, 2)
