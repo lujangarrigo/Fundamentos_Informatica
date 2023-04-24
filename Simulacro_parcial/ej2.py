4dbc89b235c83a85421f87b796d9972f9b71b078
lujangarrigo <garrigo.lujan@gmail.com>
2023-04-24 12:57:45 -0300

    ejercicio 2 terminado

--- Simulacro_parcial/ej2.py
+++ Simulacro_parcial/ej2.py
@@ -1,21 +1,12 @@
 import os, glob, re
 #a)
 
-def ejercicio_mails():
-    os.chdir('informes')
-    txt = glob.glob("*.txt")
-    mails = [] 
-    for archivo in txt: 
-        with open(archivo,"r") as file:
-            file_complete = file.read 
-            mails.append(re.findall('^[a-z0-9]+[.-]?[a-z0-9]+[.-]?\w*@[a-z]+[.][a-z]+[.]?[a-z]?$',file_complete))
-    os.mkdir("Mails")
-    with open("Mails/base_de_datos.txt","w") as salida:
-        for archivo in txt:
-            with open(archivo,"r") as file:
-                salida.write(file.readlines())
-    return mails
-
-lista = ejercicio_mails()
-print(lista)
-
+os.chdir('informes')
+txt = glob.glob("*.txt")
+with open("base_de_datos.txt", 'a') as arch: 
+        for f in txt: 
+            with open(f, 'r') as archivo_secreto: 
+                texto = archivo_secreto.read()
+                lista = re.findall('[a-z0-9]+[.-]?[a-z0-9]+[.-]?\w*@[a-z]+[.][a-z]+[.]?[a-z]?', texto)
+                for email in lista: 
+                    arch.write(email + ', ')
