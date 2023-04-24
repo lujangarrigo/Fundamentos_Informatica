13f94611670c4de7543b752bd33981de88694f2d
lujangarrigo <garrigo.lujan@gmail.com>
2023-04-24 10:30:27 -0300

    intentos ej2

--- Simulacro_parcial/ej2.py
+++ Simulacro_parcial/ej2.py
@@ -5,10 +5,10 @@ def ejercicio_mails():
     txt = glob.glob("*.txt")
     mails = [] 
     for archivo in txt: 
-        with open(archivo,"r") as file: #abrimos los archivos
+        with open(archivo,"r") as file:
             file_complete = file.read 
             mails = re.findall('^[a-z0-9]+[.-]?[a-z0-9]+[.-]?\w*@[a-z]+[.][a-z]+[.]?[a-z]?$',file_complete)
-    with open("base_de_datos.txt","w") as salida: #usamos salida cuando abrimos un archivo.txt que vamos a escribir
+    with open("base_de_datos.txt","w") as salida:
         for archivo in txt:
             with open(archivo,"r") as file:
                 salida.write(file.readlines())
@@ -16,3 +16,12 @@ def ejercicio_mails():
 
 lista = ejercicio_mails()
 print(lista)
+
+"""txt = glob.glob("*.txt")
+print(txt)
+for archivo in txt:
+    with open(archivo,"r") as file:
+        file_complete = file.read()
+        print(file_complete)
+        print(re.findall('^[a-z0-9]+[.-]?[a-z0-9]+[.-]?\w*@[a-z]+[.][a-z]+[.]?[a-z]?$',file_complete))
+"""
